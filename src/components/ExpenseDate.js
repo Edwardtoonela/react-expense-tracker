c0338f3874632355674f6b229befcef9cdb50a92
Edward Toonela <edward.toonela@icloud.com>
2022-05-23 10:32:15 +0300

    import-export for item and date components

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,4 +1,4 @@
-function ExpenseItem(props) {
+function ExpenseDate(props) {
   const month = props.date.toLocaleString('en-US', {month: 'long'});
   const day = props.date.toLocaleString('en-US', {day: '2-digit'});
   const year = props.date.getFullYear();
@@ -11,4 +11,4 @@ function ExpenseItem(props) {
   </div>
   );
 }
-export default ExpenseItem;
+export default ExpenseDate;
